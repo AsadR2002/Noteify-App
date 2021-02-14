9a44dfc1dc7ac8ccf47bd008e1ce3f87fa5d61c7
Asad Rehman <rehman.asad2002@gmail.com>
2021-02-14 15:55:28 -0500

    Material ui

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -34,7 +34,7 @@ function CreateArea(props) {
 
     return(
         <div>
-            <form>
+            <form className = "create-note">
                 <input name = "title" onChange = {handleChange} value = {note.title} placeholder = "Title"/>
                 <textarea name = "content" onChange = {handleChange} value = {note.content} placeholder = "Start new note" rows = "3"/>
                 <button onClick = {submitNote}>Add</button>
